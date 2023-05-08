f0f86aa7ae44d8501599dc966df5e12fff71b522
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2023-05-08 14:48:53 -0400

    proyecto finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,72 @@
+//Objeto es Cipher
 const cipher = {
-  // ...
+  //Metodo encode
+
+  encode: function (offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError("Wrong argument types");
+    }
+    let palabraEncriptada = "";
+    for (let i = 0; i < string.length; i++) {
+      let codigoChar = string.charCodeAt(i);
+      if (codigoChar >= 65 && codigoChar <= 90) {
+        codigoChar = ((codigoChar - 65 + offset) % 26) + 65;
+      } else if (codigoChar >= 97 && codigoChar <= 122) {
+        codigoChar = ((codigoChar - 97 + offset) % 26) + 65;
+      }
+      palabraEncriptada += String.fromCharCode(codigoChar);
+    }
+    return palabraEncriptada;
+  },
+
+  //Metodo decode
+
+  decode: function (offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw new TypeError("Wrong argument types");
+    }
+    let palabraDesencriptada = "";
+    for (let i = 0; i < string.length; i++) {
+      let codigoChar = string.charCodeAt(i);
+      if (codigoChar >= 65 && codigoChar <= 90) {
+        codigoChar = ((codigoChar - 65 - (offset % 26) + 26) % 26) + 65;
+      } //else if (codigoChar >= 97 && codigoChar <= 122) {
+      //codigoChar = ((codigoChar - 97 - (offset % 26) + 26) % 26) + 65;
+      //}
+      //console.log(codigoChar);
+      palabraDesencriptada += String.fromCharCode(codigoChar);
+    }
+    return palabraDesencriptada;
+  },
 };
 
+//const encode = (offset, palabra) => {
+// let encriptarPalabra = "";
+// for (let i = 0; i < palabra.length; i++){
+//  let reglaAscii = palabra.charCodeAt(i);
+// if (reglaAscii >= 65 && reglaAscii >= 90){
+//  let valorAscii = ((reglaAscii - 65 + offset) % 26) + 65;
+// encriptarPalabra+= String.fromCharCode(valorAscii);
+// }
+// }
+//return encriptarPalabra;
+
+//};
 export default cipher;
+
+//function btnEncriptar(){
+//const textoEncriptado = encriptar(inputTexto.value)
+//mensaje.value = textoEncriptado;
+//}
+
+//function encriptar(stringEncriptada){
+
+//stringEncriptada = stringEncriptada.toUpperCase();
+
+//for(let i = 0; i < matrizCodigo.length; i++){
+// if(stringEncriptada.includes()){
+//    stringEncriptada = stringEncriptada.replaceAll(String.fromCharCode(), ;
+//}
+//}
+//return stringEncriptada;
+//};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+const encriptar = document.getElementById("btnEncriptar");
+
+encriptar.addEventListener("click", function () {
+  const offset = parseInt(document.getElementById("offset").value);
+  const string = document.querySelector(".input-texto").value;
+  const palabraEncriptada = document.getElementById("resultado");
+
+  palabraEncriptada.textContent = cipher.encode(offset, string);
+});
+
+const desencriptar = document.getElementById("btnDesencriptar");
+
+desencriptar.addEventListener("click", function () {
+  const offset = parseInt(document.getElementById("offset").value);
+  const string = document.querySelector(".input-texto").value;
+  const palabraDesencriptada = document.getElementById("resultado");
+
+  palabraDesencriptada.textContent = cipher.decode(offset, string);
+});
+//console.log(cipher);
+
+//Prueba con consola
+//const string = "HOLA";
+//const offset = 5;
+//const encriptarMensaje = cipher.encode(offset, string);
+//console.log(encriptarMensaje);
+
+//const desencriptarMensaje = cipher.decode(offset, string);
+//console.log(desencriptarMensaje);
